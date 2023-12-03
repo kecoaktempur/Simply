6381a858c2dc5ce3f6088ded29b2814d413f2d5c
kecoaktempur <triwan98@gmail.com>
2023-12-03 22:22:32 +0700

    base footer navbar html done

--- Simply/urls.py
+++ Simply/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Simply/', include('Simply_Client.urls'))
+    # path('admin/', admin.site.urls),
+    path('', include('Simply_Client.urls'))
 ]
