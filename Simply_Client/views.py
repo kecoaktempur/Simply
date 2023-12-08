e863348fc5f0435d47241e125dccf1256d960f0a
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-12-08 09:55:58 +0700

    setting

--- Simply_Client/views.py
+++ Simply_Client/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def qris(request):
+    return render(request, 'qris.html')
+
+def upload(request):
+    return render(request, 'upload.html')
