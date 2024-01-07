50c17ee84a11f21390a87d5f8636e01e494fd23d
kecoaktempur <triwan98@gmail.com>
2024-01-08 01:30:10 +0700

    patch: done and active state management done

--- Simply_Client/views.py
+++ Simply_Client/views.py
@@ -7,55 +7,47 @@ def home(request):
     return render(request, 'home.html')
 
 
-def tracking(request):
-    active_link = 'tracking'
-    done_state = True
-    return render(request, 'tracking.html', {'active_link': active_link, 'done_state': done_state})
-
-
-def trackingDone(request):
-    return render(request, 'tracking2.html')
-
-
-def qris(request):
-    return render(request, 'qris.html')
-
-
-def upload(request):
-    active_link = 'upload'
-    done_state = True
-    return render(request, 'upload.html', {'active_link': active_link, 'done_state': done_state})
-
-
-def trackingDone(request):
-    return render(request, 'tracking2.html')
-
-
-def qris(request):
-    active_link = 'payment'
-    done_state = True
-    return render(request, 'qris.html', {'active_link': active_link, 'done_state': done_state})
-
-
 def formRegis(request):
     active_link = 'registration'
-    done_state = True
+    done_state = False
     return render(request, 'form-reg.html', {'active_link': active_link, 'done_state': done_state})
 
 
 def payment(request):
     active_link = 'payment'
-    done_state = True
+    done_state = False
     return render(request, 'payment-opt.html', {'active_link': active_link, 'done_state': done_state})
+
+
+def qris(request):
+    active_link = 'payment'
+    done_state = False
+    return render(request, 'qris.html', {'active_link': active_link, 'done_state': done_state})
+
+
+def upload(request):
+    active_link = 'upload'
+    done_state = False
+    return render(request, 'upload.html', {'active_link': active_link, 'done_state': done_state})
 
 
 def validation(request):
     active_link = 'validation'
-    done_state = True
+    done_state = False
     return render(request, 'validation.html', {'active_link': active_link, 'done_state': done_state})
 
 
 def schedule(request):
     active_link = 'schedule'
-    done_state = True
+    done_state = False
     return render(request, 'schedule.html', {'active_link': active_link, 'done_state': done_state})
+
+
+def tracking(request):
+    active_link = 'tracking'
+    done_state = False
+    return render(request, 'tracking.html', {'active_link': active_link, 'done_state': done_state})
+
+
+def trackingDone(request):
+    return render(request, 'tracking2.html')
